668bd558216042389d54933ef7a48b3fe3b9e5ef
mobiuschang <mobiuschang@gmail.com>
2016-12-04 19:18:35 -0500

    Set up webpack and home component

---
+++ browser/components/Home.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Home = (props) => {
+  return (
+    <div>Home.jsx rendered</div>
+  )
+}
+
+module.exports = Home;
---
+++ browser/main.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+import Home from './components/Home';
+
+ReactDOM.render(
+  <Home />,
+  document.getElementById('main')
+)
