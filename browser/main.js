e00deb6a87ca9d342ff4c570c0f345253b8ce1d2
mobiuschang <mobiuschang@gmail.com>
2016-12-09 00:26:48 -0500

    Set up basic react/redux

--- browser/main.js
+++ browser/main.js
@@ -1,8 +1,19 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+
 import Home from './components/Home';
 
-ReactDOM.render(
-  <Home />,
+import store from './store';
+
+render(
+  <Provider store={store}>
+    <Router history = { browserHistory } >
+      <Route path = '/'>
+        <IndexRoute component = {Home} />
+      </Route>
+    </Router>
+  </Provider>,
   document.getElementById('main')
-)
+);
